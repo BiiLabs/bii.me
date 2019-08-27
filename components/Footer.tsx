22e02c9da30b836298a5625d3e175493cda4c375
JHIH-CING LI <jhihcing.li@gmail.com>
2019-08-27 16:55:30 +0800

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,34 +1,72 @@
 import { Row, Col } from 'antd';
 import { colors } from '@/layouts/globalStyles';
-import { getRelativePath } from '@/utils';
+import { getRelativePath, media } from '@/utils';
+import styled from 'styled-components';
 
 interface IMedia {
   uri: string;
   img: string;
 }
 
+const MediaIcon = styled.img`
+  width: 32px;
+  ${media('md')} {
+    width: 48px;
+  }
+`;
+
 const MediaLink: React.FC<IMedia> = ({ uri, img }) => (
   <a style={{ marginRight: 16 }} href={uri}>
-    <img style={{ width: 32 }} src={img} />
+    <MediaIcon src={img} />
   </a>
 );
 
+const MediaLinkWrapper = styled(Row)`
+  display: flex;
+  justify-content: flex-end;
+  ${media('md')} {
+    justify-content: center;
+  }
+`;
+
+const Logo = styled.img`
+  width: 150px;
+  ${media('md')} {
+    width: 80px;
+    margin-bottom: 20px;
+  }
+`;
+
+const Container = styled.div`
+  padding: 32px 20px 14px;
+  ${media('md')} {
+    padding: 16px 0 0 0;
+  }
+`;
+
+const CopyRight = styled.p`
+  text-align: center;
+  margin-top: 100px;
+  color: #fff;
+  font-family: 'HelveticaNeue-Light';
+  ${media('md')} {
+    margin-top: 32px;
+    font-size: 16px;
+  }
+`;
+
 const Footer: React.FC = () => (
-  <div
+  <Container
     style={{
-      padding: '32px 20px 14px',
       backgroundColor: colors.primary,
     }}
   >
     <Row type="flex" justify="space-between">
-      <Col push={2}>
-        <img
-          style={{ width: 150 }}
-          src={getRelativePath('/static/logo-biilabs.svg')}
-        />
+      <Col xs={24} sm={24} md={2} style={{ textAlign: 'center' }}>
+        <Logo src={getRelativePath('/static/logo-biilabs.svg')} />
       </Col>
-      <Col>
-        <Row>
+      <Col xs={24} sm={24} md={22}>
+        <MediaLinkWrapper>
           <MediaLink
             uri="https://biilabs.io/"
             img={getRelativePath('/static/link-earth.svg')}
@@ -45,15 +83,11 @@ const Footer: React.FC = () => (
             uri="https://medium.com/biilabs"
             img={getRelativePath('/static/link-medium.svg')}
           />
-        </Row>
+        </MediaLinkWrapper>
       </Col>
     </Row>
-    <Row style={{ marginTop: 100 }} type="flex" justify="center">
-      <p style={{ color: '#fff', fontFamily: 'HelveticaNeue-Light' }}>
-        2019 © BiiLabs
-      </p>
-    </Row>
-  </div>
+    <CopyRight>2019 © BiiLabs</CopyRight>
+  </Container>
 );
 
 export default Footer;
