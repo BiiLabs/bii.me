64660689cafe930c9d60623cd3ed9310a2f448d9
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-08-27 18:49:13 +0800

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a57e26572818958e0560";
+/******/ 	var hotCurrentHash = "ba4c6f5e098042d76865";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/index.js.237d859eed28d746d3f2.hot-update.js
@@ -0,0 +1,94 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Header.tsx":
+/*!*******************************!*\
+  !*** ./components/Header.tsx ***!
+  \*******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var antd_lib_row_style__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! antd/lib/row/style */ "./node_modules/antd/lib/row/style/index.js");
+/* harmony import */ var antd_lib_row_style__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(antd_lib_row_style__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var antd_lib_row__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! antd/lib/row */ "./node_modules/antd/lib/row/index.js");
+/* harmony import */ var antd_lib_row__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(antd_lib_row__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var antd_lib_col_style__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! antd/lib/col/style */ "./node_modules/antd/lib/col/style/index.js");
+/* harmony import */ var antd_lib_col_style__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(antd_lib_col_style__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var antd_lib_col__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! antd/lib/col */ "./node_modules/antd/lib/col/index.js");
+/* harmony import */ var antd_lib_col__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(antd_lib_col__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var antd_lib_layout_style__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! antd/lib/layout/style */ "./node_modules/antd/lib/layout/style/index.js");
+/* harmony import */ var antd_lib_layout_style__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(antd_lib_layout_style__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var antd_lib_layout__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! antd/lib/layout */ "./node_modules/antd/lib/layout/index.js");
+/* harmony import */ var antd_lib_layout__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(antd_lib_layout__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../utils */ "./utils.ts");
+/* harmony import */ var _Menu__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./Menu */ "./components/Menu.tsx");
+
+
+
+
+
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_6___default.a.createElement;
+
+
+
+var StyledHeader = Object(styled_components__WEBPACK_IMPORTED_MODULE_7__["default"])(antd_lib_layout__WEBPACK_IMPORTED_MODULE_5___default.a.Header).withConfig({
+  displayName: "Header__StyledHeader",
+  componentId: "kyvnhs-0"
+})(["z-index:2;width:100%;background-color:transparent;position:absolute;height:auto;padding:14px;display:flex;justify-content:space-between;flex-wrap:wrap;align-items:center;line-height:initial;"]);
+var StyledImage = styled_components__WEBPACK_IMPORTED_MODULE_7__["default"].img.withConfig({
+  displayName: "Header__StyledImage",
+  componentId: "kyvnhs-1"
+})(["width:27px;"]);
+var NavTitle = styled_components__WEBPACK_IMPORTED_MODULE_7__["default"].span.withConfig({
+  displayName: "Header__NavTitle",
+  componentId: "kyvnhs-2"
+})(["display:inline-block;font-size:24px;margin-left:10px;color:rgb(28,42,77);"]);
+var A = styled_components__WEBPACK_IMPORTED_MODULE_7__["default"].a.withConfig({
+  displayName: "Header__A",
+  componentId: "kyvnhs-3"
+})(["margin-left:34px;color:#fff;font-size:20px;"]);
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return __jsx(react__WEBPACK_IMPORTED_MODULE_6___default.a.Fragment, null, __jsx(_Menu__WEBPACK_IMPORTED_MODULE_9__["Menu"], null), __jsx(StyledHeader, null, __jsx(antd_lib_row__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    type: "flex",
+    justify: "space-between",
+    style: {
+      width: '100%'
+    }
+  }, __jsx(antd_lib_col__WEBPACK_IMPORTED_MODULE_3___default.a, {
+    style: {
+      display: 'flex',
+      alignItems: 'center'
+    }
+  }, __jsx(StyledImage, {
+    src: Object(_utils__WEBPACK_IMPORTED_MODULE_8__["getRelativePath"])('/static/icon-4.svg'),
+    alt: "logo"
+  }), __jsx(NavTitle, null, "BiiMe")), __jsx(antd_lib_col__WEBPACK_IMPORTED_MODULE_3___default.a, {
+    xs: 0,
+    sm: 0,
+    md: 20,
+    style: {
+      textAlign: 'end'
+    }
+  }, __jsx(A, {
+    href: "#features"
+  }, "Features"), __jsx(A, {
+    href: "#scenarios"
+  }, "Scenarios"), __jsx(A, {
+    href: "#how-it-work"
+  }, "How it works"), __jsx(A, {
+    href: "#download"
+  }, "Download"), __jsx(A, {
+    href: "https://biilabs.io/"
+  }, "BiiLabs")))));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.237d859eed28d746d3f2.hot-update.js.map
